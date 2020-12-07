5718ca0937b0515f4b9554daaae763c1734d7240
yusuke.ishio <yusuke1403@gmail.com>
2020-12-07 08:02:47 +0000

    validation check for id

--- src/components/pages/login.js
+++ src/components/pages/login.js
@@ -1,29 +1,55 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styles from './login.module.scss';
 import SYSTEM_CONST from '../../system_const';
 import Button from '../atoms/button';
 
+const ID = {
+  MIN: 1,
+  MAX: 8,
+};
+
 const Login = () => {
 
-  // 認証
+  const [id, setID] = useState('');
+
+
+  // init
+  useEffect( ()=> {
+    console.log('change id');
+  }, [id]);
+
+
+  // auth
   const onClickLogin = () => {
-    // console.log('auth');
     location.href = SYSTEM_CONST.URL.TOP;
   };
+
+
+  // change
+  const onChangeIDHandler = e =>
+    setID(e.currentTarget.value);
 
 
   return (
     <>
       <h1 className={styles.title}>ログイン</h1>
 
+
+      {
+        ID.MAX <= id.length ?
+          <p className={styles.warning}>{ID.MAX}文字以下で入力してください</p> :
+          ''
+      }
+      
+
       <dl className={styles.form}>
         <dt>id</dt>
-        <dd><input type="text" placeholder="id" className="form-control" /></dd>
+        <dd><input type="text" placeholder="id" className="form-control" onChange={onChangeIDHandler} /></dd>
       </dl>
 
       <dl className={`${styles.form} `}>
         <dt>password</dt>
-        <dd><input type="password" placeholder="password" className="form-control"/></dd>
+        <dd><input type="password" placeholder="password" className="form-control" /></dd>
       </dl>
 
       <div className={styles.box}>
